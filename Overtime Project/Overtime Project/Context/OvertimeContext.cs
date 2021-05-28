7dcbf12e13d4ba3debf22e8953b97cef5385c757
Abror-1221 <muliausman12@gmail.com>
2021-05-28 15:58:24 +0700

    Add Context + Models

---
+++ Overtime Project/Overtime Project/Context/OvertimeContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Overtime_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Overtime_Project.Context
+{
+    public class OvertimeContext : DbContext
+    {
+        public OvertimeContext(DbContextOptions<OvertimeContext> options) : base(options)
+        {
+
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseLazyLoadingProxies();
+        //}
+
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Account> Account { get; set; }
+        public DbSet<RoleAccount> RoleAccount { get; set; }
+        public DbSet<Role> Role { get; set; }
+        public DbSet<Overtime> Overtime { get; set; }
+        public DbSet<Kind> Kind { get; set; }
+        public DbSet<Status> Status { get; set; }
+    }
+}
---
+++ Overtime Project/Overtime Project/Models/RoleAccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Overtime_Project.Models
+{
+    public class RoleAccount
+    {
+        public string NIK { get; set; }
+        public int RoleId { get; set; }
+    }
+}
