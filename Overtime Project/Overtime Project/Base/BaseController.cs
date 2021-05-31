091263e5d9852ac3b9302eee357b90a9c9948dec
Abror-1221 <muliausman12@gmail.com>
2021-05-31 10:43:38 +0700

    Add Repository + Controller (BaseController & Controllers)

---
+++ Overtime Project/Overtime Project/Base/BaseController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Overtime_Project.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Overtime_Project.Base
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController<Entity, Repository, Key> : ControllerBase
+        where Entity : class
+        where Repository : IRepository<Entity, Key>
+    {
+        private readonly Repository repo;
+        public BaseController(Repository repository)
+        {
+            this.repo = repository;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Entity entity)
+        {
+            try
+            {
+                repo.Insert(entity);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(400, new { status = HttpStatusCode.Forbidden, message = $"Error : Column name doesn't match or PK column is null!!" });
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            IEnumerable<Entity> entities = repo.Get();
+            return Ok(entities);
+        }
+
+        [HttpGet("{Key}")]
+        public ActionResult Get(Key key)
+        {
+            var entity = repo.Get(key);
+            if (entity != null)
+            {
+                return Ok(entity);
+            }
+            else
+            {
+                return NotFound($"Error : {key} not found!!");
+            }
+        }
+
+        [HttpDelete("{Key}")]
+        public ActionResult Delete(Key key)
+        {
+            var entity = repo.Get(key);
+            if (entity != null)
+            {
+                return Ok(repo.Delete(key));
+            }
+            else
+            {
+                return NotFound($"Error : The {key} that wanted to delete was not found!!");
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Update(Entity entity)
+        {
+            try
+            {
+                repo.Update(entity);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(400, new { status = HttpStatusCode.NotModified, message = "Error : Data not updated" });
+            }
+        }
+    }
+}
---
+++ Overtime Project/Overtime Project/Repository/Data/KindRepository.cs
@@ -0,0 +1,17 @@
+﻿using Overtime_Project.Context;
+using Overtime_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Overtime_Project.Repository.Data
+{
+    public class KindRepository : GeneralRepository<OvertimeContext, Kind, int>
+    {
+        public KindRepository(OvertimeContext conn) : base(conn)
+        {
+
+        }
+    }
+}
